320f5c3f5371e107d6a94a2684b1de661e00cffd
Linh Hoang <ellehoang@hotmail.com>
2022-01-16 10:19:58 -0500

    create data to populate to database

--- backend/db/schema/02_posts.sql
+++ backend/db/schema/02_posts.sql
@@ -1,8 +1,37 @@
-DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS flights CASCADE;
+DROP TABLE IF EXISTS favourites CASCADE;
+DROP TABLE IF EXISTS activities CASCADE;
 
-CREATE TABLE posts(
+-- Parameters from FLIGHTS SPECIAL OFFERS
+CREATE TABLE flights(
     id SERIAL PRIMARY KEY NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    content TEXT,
-    user_id INTEGER REFERENCES users(id)
+    origin VARCHAR(3) NOT NULL,
+    origin_name VARCHAR(250) NOT NULL,
+    origin_airport VARCHAR(3) NOT NULL,
+    destination VARCHAR(3) NOT NULL,
+    destination_name VARCHAR(250) NOT NULL,
+    destination_airport VARCHAR(3) NOT NULL,
+    price INTEGER NOT NULL,
+    airline VARCHAR(2) NOT NULL,
+    flight_number VARCHAR(4) NOT NULL,
+    departure_at DATETIME,
+    return_at DATETIME,
+    transfers INTEGER,
+    return_transfers INTEGER,
+    duration INTEGER NOT NULL,
+    link TEXT
+);
+
+CREATE TABLE favourites (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    flight_id INTEGER REFERENCES flights(id) ON DELETE CASCADE
+);
+
+CREATE TABLE activities (
+    id SERIAL PRIMARY KEY NOT NULL,
+    favourite_id INTEGER REFERENCES favourites(id) ON DELETE CASCADE,
+    name VARCHAR(250),
+    start_at DATETIME,
+    end_at DATETIME
 );
