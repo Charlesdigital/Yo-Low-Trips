582ace3731d0d3eb84bb52885069d9681d784539
Linh Hoang <ellehoang@hotmail.com>
2022-01-16 21:52:22 -0500

    fixed schema and seeds to reformat parameters and add cities table

--- backend/db/schema/02_posts.sql
+++ backend/db/schema/02_posts.sql
@@ -1,25 +1,19 @@
 DROP TABLE IF EXISTS flights CASCADE;
 DROP TABLE IF EXISTS favourites CASCADE;
 DROP TABLE IF EXISTS activities CASCADE;
+DROP TABLE IF EXISTS cities CASCADE;
 
--- Parameters from FLIGHTS SPECIAL OFFERS
-CREATE TABLE flights(
+-- Parameters from Cheapest Tickets
+CREATE TABLE flights (
     id SERIAL PRIMARY KEY NOT NULL,
     origin VARCHAR(3) NOT NULL,
-    origin_name VARCHAR(250) NOT NULL,
-    origin_airport VARCHAR(3) NOT NULL,
     destination VARCHAR(3) NOT NULL,
-    destination_name VARCHAR(250) NOT NULL,
-    destination_airport VARCHAR(3) NOT NULL,
     price INTEGER NOT NULL,
     airline VARCHAR(2) NOT NULL,
     flight_number VARCHAR(4) NOT NULL,
     departure_at TIMESTAMP,
     return_at TIMESTAMP,
-    transfers INTEGER,
-    return_transfers INTEGER,
-    duration INTEGER NOT NULL, -- in minutes
-    link TEXT
+    expires_at TIMESTAMP
 );
 
 CREATE TABLE favourites (
@@ -34,4 +28,9 @@ CREATE TABLE activities (
     name VARCHAR(250),
     start_at TIMESTAMP,
     end_at TIMESTAMP
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(250)
 );
--- backend/db/seeds/02_posts.sql
+++ backend/db/seeds/02_posts.sql
@@ -1,44 +1,54 @@
--- Parameters from FLIGHTS SPECIAL OFFERS
+-- Parameters from Cheapest Tickets
 INSERT INTO
-flights(id, origin, origin_name, destination, destination_name, origin_airport, destination_airport, price, airline, flight_number, departure_at, return_at, transfers, return_transfers, duration, link)
+flights(origin, destination, price, airline, flight_number, departure_at, return_at, expires_at)
 VALUES
 -- Toronto to Japan
-    (1, 'YTO', 'Toronto', 'TYO', 'Tokyo', 'YYZ', 'NRT', 200, 'OZ', '111', '2022-01-28T08:20:00-05:00', '2022-02-02T10:00:00-05:00', 0, 0, 780, 'https://en.wikipedia.org/wiki/Japan'),
+    ('YYZ', 'TYO', 200, 'OZ', '111', '2022-01-31T08:20:00Z', '2022-02-12T10:00:00Z', '2022-01-28T13:30:00Z'),
 -- Toronto to Paris
-    (2, 'YTO', 'Toronto', 'PAR', 'Paris', 'YYZ', 'CDG', 175, 'AC', '222', '2022-01-28T00:20:00-05:00', '2022-01-31T13:45:00-05:00', 1, 1, 480, 'https://en.wikipedia.org/wiki/Paris'),
+    ('YYZ', 'PAR', 175, 'AC', '222', '2022-01-30T00:20:00Z', '2022-02-07T13:45:00Z', '2022-01-29T16:00:00Z'),
 -- Toronto to Sao Paulo
-    (3, 'YTO', 'Toronto', 'SAO', 'Sao Paulo', 'YYZ', 'GRU', 100, 'UA', '333', '2022-01-29T17:41:00-05:00', '2022-02-05T14:14:00-05:00', 2, 2, 1200, 'https://en.wikipedia.org/wiki/S%C3%A3o_Paulo'),
+    ('YTZ','SAO', 100, 'UA', '333', '2022-02-03T17:41:00Z', '2022-02-10T14:14:00Z', '2022-01-28T13:30:00Z'),
 -- Toronto to Cairo
-    (4, 'YTO', 'Toronto', 'CAI', 'Cairo International Airport', 'YYZ', 'CAI', 125, 'EK', '444', '2022-01-30T20:15:00-05:00', '2022-02-09T18:23:00-05:00', 1, 1, 930, 'https://en.wikipedia.org/wiki/Cairo'),
+    ('YYZ', 'CAI', 125, 'EK', '444', '2022-02-01T20:15:00Z', '2022-02-09T18:23:00Z', '2022-01-29T09:00:00Z'),
 -- Toronto to Honolulu
-    (5, 'YTO', 'Toronto', 'HNL', 'Honolulu', 'YYZ', 'HNL', 200, 'AC', '555', '2022-01-29T19:00:00-05:00', '2022-02-07T16:01:00-05:00', 1, 1, 780, 'https://en.wikipedia.org/wiki/Honolulu'),
+    ('YYZ', 'HNL', 150, 'AC', '555', '2022-02-02T19:00:00Z', '2022-02-14T16:01:00Z', '2022-01-30T00:00:00Z'),
+-- Toronto to Sweden
+    ('YYZ', 'ARN', 75, 'TP', '666', '2022-02-01T17:30:00Z', '2022-02-08T13:48:00Z', '2022-01-27T22:30:00Z'),
 -- Vancouver to London
-    (6, 'YVR', 'Vancouver', 'LON', 'London', 'YVR', 'LHR', 150, 'AC', '666', '2022-01-29T16:50:00-05:00', '2022-02-03T10:10:00-05:00', 0, 0, 540, 'https://en.wikipedia.org/wiki/London'),
+    ('YVR', 'LON', 150, 'AC', '666', '2022-01-31T16:50:00Z', '2022-02-11T10:10:00Z', '2022-01-28T08:30:00Z'),
 -- Vancouver to Las Vegas
-    (7, 'YVR', 'Vancouver', 'LAS', 'Las Vegas', 'YVR', 'LAS', 75, 'AC', '777', '2022-01-28T11:30:00-05:00', '2022-02-02T18:11:00-05:00', 0, 0, 163, 'https://en.wikipedia.org/wiki/Las_Vegas'),
--- Vancouver to Delhi (NEED DURATION)
-    (8, 'YVR', 'Vancouver', 'DEL', 'Delhi', 'YVR', 'DEL', 125, 'BA', '888', '2022-01-31T22:55:00-05:00', '2022-02-13T10:04:00-05:00', 1, 1, 1800, 'https://en.wikipedia.org/wiki/Delhi');
+    ('YVR', 'LAS', 75, 'AC', '777', '2022-01-29T11:30:00Z', '2022-02-05T18:11:00Z', '2022-01-28T02:00:00Z'),
+-- Vancouver to Delhi
+    ('YVR', 'DEL', 125, 'BA', '888', '2022-01-31T22:55:00Z', '2022-02-13T10:04:00Z', '2022-01-27T20:30:00Z');
 
-INSERT INTO favourites(id, flight_id, user_id)
+INSERT INTO favourites(flight_id, user_id)
 VALUES
 -- Amanda Won's favourite flights
-    (1, 1, 1), -- Toronto to Japan
-    (2, 2, 1), -- Toronto to Paris
-    (3, 5, 1), -- Toronto to Honolulu
+    (1, 1), -- Toronto to Japan
+    (2, 1), -- Toronto to Paris
+    (5, 1), -- Toronto to Honolulu
 -- KC Trey's favourite flights
-    (4, 7, 3); -- Vancover to Las Vegas
+    (8, 3); -- Vancover to Las Vegas
 
 
 INSERT INTO activities(favourite_id, name, start_at, end_at)
 VALUES
 -- Japan
-    (1, 'Visit Tokyo Tower', '2022-01-29T11:00:00', '2022-01-29T13:00:00'),
-    (1, 'Attend a Tea Ceremony', '2022-01-29T14:00:00', '2022-01-29T16:00:00'),
-    (1, 'Try Ramen Noodles', '2022-01-29T18:00:00', '2022-01-30T19:00:00'),
-    (1, 'Visit a Hot Spring', '2022-01-30T17:00:00', '2022-01-31T11:00:00'),
+    (1, 'Visit Tokyo Tower', '2022-02-02T11:00:00', '2022-02-02T13:00:00'),
+    (1, 'Attend a Tea Ceremony', '2022-02-03T14:00:00', '2022-02-03T16:00:00'),
+    (1, 'Try Ramen Noodles', '2022-02-05T18:00:00', '2022-02-05T19:00:00'),
+    (1, 'Visit a Hot Spring', '2022-02-06T17:00:00', '2022-02-07T11:00:00'),
 -- Honolulu
-    (3, 'Try Hula Dancing', '2022-02-01T:15:00:00', '2022-02-01T16:00:00'),
     (3, 'Go Snorkeling', '2022-02-03T10:00:00', '2022-02-03T11:00:00'),
+    (3, 'Try Hula Dancing', '2022-02-03T:15:00:00', '2022-02-03T16:00:00'),
 -- Las Vegas
-    (4, 'Visit the Hover Dam', '2022-01-30T09:00:00', '2022-01-30T13:00:00'),
-    (4, 'See a Cirque du Soleil show', '2022-01-30T22:00:00', '2022-01-31T00:0000');
+    (4, 'Visit the Hover Dam', '2022-01-31T09:00:00', '2022-01-31T13:00:00'),
+    (4, 'See a Cirque du Soleil show', '2022-01-31T22:00:00', '2022-02-01T00:0000');
+
+INSERT INTO cities(name)
+VALUES
+-- Canada
+    ('Vancouver'),
+    ('Calgary'),
+    ('Toronto'),
+    ('Montreal');
