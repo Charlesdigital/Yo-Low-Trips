763dc5788509faf73cb91274b7f21842b497d2eb
Anshaal Hussain <anshaalhussain@gmail.com>
2022-01-19 12:31:15 -0500

    backend route files

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,39 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const flightRouter = require("./routes/flights")
+const favouritesRouter = require("./routes/favourites")
 
-const db = require("./db")
-const flights = require("./routes/flightsRoute")
+const app = express();
+//const flightRoutes = require("./routes/flightsRoute");
 
-app.use(logger('dev'));
+const db = require("./db");
+const helpers = require("./helpers/dbHelpers")(db);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
-//index router
-app.use("/index", indexRouter)
+// //index router
+// app.use("/index", indexRouter)
 
+//tutorial
+//console.log("HELPERS", helpers)
+
+
+app.use("/flights", flightRouter(db));
+
+app.use("/favourites", favouritesRouter(db));
 
 
 module.exports = app;
--- backend/helpers/dbHelpers.js
+++ backend/helpers/dbHelpers.js
@@ -1,76 +1,94 @@
 module.exports = (db) => {
-    // Get a single user from database given their id
-    const getUsers = () => {
-        const query = {
-            text: `SELECT * FROM users WHERE id = $1`,
-        }
-
-        return db
-            .query(query)
-            .then((result) => result.rows)
-            .catch((err) => err);
+  // Get a single user from database given their id
+  const getUsers = (id) => {
+    const query = {
+      text: `SELECT * FROM users WHERE id = $1`,
     };
-    
-    // Get a single user from database given their email
-    const getUserByEmail = email => {
 
-        const query = {
-            text: `SELECT * FROM users WHERE email = $1` ,
-            values: [email]
-        }
+    return db
+      .query(query, [id])
+      .then((result) => result.rows)
+      .catch((err) => err);
+  };
 
-        return db
-            .query(query)
-            .then(result => result.rows[0])
-            .catch((err) => err);
-    }
-    
-    // Add user to database
-    const addUser = (firstName, lastName, email, password) => {
-        const query = {
-            text: `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *` ,
-            values: [firstName, lastName, email, password]
-        }
+  // Get a single user from database given their email
+  const getUserByEmail = (email) => {
+    const query = {
+      text: `SELECT * FROM users WHERE email = $1`,
+      values: [email],
+    };
 
-        return db.query(query)
-            .then(result => result.rows[0])
-            .catch(err => err);
-    }
+    return db
+      .query(query, [email])
+      .then((result) => result.rows[0])
+      .catch((err) => err);
+  };
 
-    const getAllFavouritesForUser = (userId) => {
-        const query = {
-            text: `SELECT * FROM favourites
-            WHERE user_id = $1`,
-        //     text: `SELECT users.id as user_id, first_name, last_name, email, posts.id as post_id, title, content
-        // FROM users
-        // INNER JOIN posts
-        // ON users.id = posts.user_id`
-        }
+  // Add user to database
+  const addUser = (firstName, lastName, email, password) => {
+    const query = {
+      text: `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *`,
+      values: [firstName, lastName, email, password],
+    };
 
-        return db.query(query)
-            .then(result => result.rows)
-            .catch(err => err);
+    return db
+      .query(query, [firstName, lastName, email, password])
+      .then((result) => result.rows[0])
+      .catch((err) => err);
+  };
 
-    }
+  const getAllFavouritesForUser = (userId) => {
+    const query = {
+      text: `SELECT * FROM favourites
+      JOIN users ON users.id = favourites.user_id
+      JOIN flights ON flights.id = favourites.flight_id
+      WHERE favourites.user_id = $1`
+      
+    //   `SELECT * FROM flights
+    //         JOIN favourites ON flights.flight_id = favourites.flight_id
+    //         WHERE favourites.user_id = $1
+    //         `
+      //     text: `SELECT users.id as user_id, first_name, last_name, email, posts.id as post_id, title, content
+      // FROM users
+      // INNER JOIN posts
+      // ON users.id = posts.user_id`
+    };
 
-    const getCityForOrigin = (id) => {
-        const query = {
-           text: `SELECT name
+    return db
+      .query(query, [userId])
+      .then((result) => result.rows)
+      .catch((err) => err);
+  };
+
+  const getCityForOrigin = (id) => {
+    const query = {
+      text: `SELECT name
             FROM cities
-            WHERE id = $1`
-        }
-        return db.query(query)
-            .then(result => result.rows)
-            .catch(err => err);
-    }
-
-    const get
-
-    return {
-        getUsers,
-        getUserByEmail,
-        addUser,
-        getAllFavouritesForUser,
-        getCityForOrigin
+            WHERE id = $1`,
     };
+    return db
+      .query(query, [id])
+      .then((result) => result.rows)
+      .catch((err) => err);
+  };
+
+  const getFlights = () => {
+    const query = {
+      text: `SELECT *
+            FROM flights`,
+    };
+    return db
+      .query(query)
+      .then((result) => result.rows)
+      .catch((err) => err);
+  };
+
+  return {
+    getUsers,
+    getUserByEmail,
+    addUser,
+    getAllFavouritesForUser,
+    getCityForOrigin,
+    getFlights,
+  };
 };
