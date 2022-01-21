d6a5ff12cb9845af54915b27615c0dfc6fce2d37
Linh Hoang <ellehoang@hotmail.com>
2022-01-20 21:52:58 -0500

    update backend routes for favourites and users

--- backend/routes/favourites.js
+++ backend/routes/favourites.js
@@ -4,19 +4,45 @@ const router = require("express").Router();
 const helpers = require("../helpers/dbHelpers")(db);
 
 module.exports = () => {
-  router.get("/", (req, res) => {
+  router.get("/favourites", (req, res) => {
     helpers.getAllFavouritesForUser(1).then((result) => {
       console.log("RESULT", result)
-      const flightData = result;
+      const favData = result;
      
       //important for CORS error
       res.header("Access-Control-Allow-Origin", "*");
       //res.status(200).json({ users })
-      res.send( flightData );
+      res.send( favData );
       // res.json("db result", { users });
     });
   });
-  
 
+  router.get("/favourites/:user_id", (req, res) => {
+    const user_id = req.params.user_id;
+    helpers.getAllFavouritesForUser(user_id).then((result) => {
+      console.log("RESULT", result)
+      const favData = result;
+     
+      //important for CORS error
+      res.header("Access-Control-Allow-Origin", "*");
+      //res.status(200).json({ users })
+      res.send( favData );
+      // res.json("db result", { users });
+    });
+  });
+
+  // POST to update Favourites Table
+  router.post("/favourites", (req, res) => {
+    const userId = req.session.userId;
+    helpers.addFavourite({...req.body, user_id: userId})
+      .then(favourite => {
+        res.send(favourite);
+      })
+      .catch(err => {
+        console.error(err);
+        res.send(err)
+      });
+  });
+  
   return router;
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const session = require("express-session");
 const db = require("../db");
 const helpers = require("../helpers/dbHelpers")(db);
 
 module.exports = function (db) {
   /** GET users listing **/
-  router.get("/", (req, res) =>
+  router.get("/users", (req, res) =>
     helpers.getUsers(1)
       .then(data => {
         const user = data;
@@ -19,17 +20,54 @@ module.exports = function (db) {
       })
   );
   
+  // check if a user exists with a given username and password
+  const login = function (email, password) {
+    return helpers.getUserByEmail(email)
+    .then(response => {
+      console.log("Database user:", response)
+      return response;
+    });
+  }
+  exports.login = login
+  
   /* Submit user data to be processed */
   router.post("/login", async (req, res) => {
     console.log('REQ BODY', req.body);
     const { email, password } = req.body;
-    const user = await db.query(`
-    SELECT email, password FROM users
-    WHERE email = $1`, [email])
-    res.json ({ user });
-  })
-   return router
-}
+    if (email && password) {
+      
+      login(email, password)
+      .then(user => {
+        if (!user) {
+          console.log("THIS SHOULD RUN********")
+          // res.send({error: "error"});
+          return res.status(404).send({ status: "Error", message: "Invalid user" }); // this will going to .catch err on front end
+        }
+        // res.redirect("favourites");
+        console.log("SHOULDNT WORK+++++++++++++", user)
+        // res.redirect("/flights")
+        return res.status(200).send({ status: "Login", message: "Welcome to Yo-low Trips!", user }); // this will go to .then
+        //req.session.userId = user.id;
+        //res.json({user: {name: user.first_name, email: user.email, id: user.id}});
+      })
+      .catch(e => res.send(e));
+
+    }
+
+    
+      // const user = await db.query(`
+      // SELECT email, password FROM users
+      // WHERE email = $1`, [email])
+  });
+
+  // Logout user
+  router.post("/logout", (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
+
+   return router;
+};
 // /* GET users listing. */
 // router.get('/', function(req, res) {
 //   res.render("index")
