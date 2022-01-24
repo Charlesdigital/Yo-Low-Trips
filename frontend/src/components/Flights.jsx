716aa3dd399b3e5ca510c719fe8e0e85e088272d
Anshaal Hussain <anshaalhussain@gmail.com>
2022-01-23 19:52:12 -0500

    destination filter functional

--- frontend/src/components/Flights.jsx
+++ frontend/src/components/Flights.jsx
@@ -10,62 +10,78 @@ import {
   Container,
   Grid,
 } from "@mui/material";
-import { useParams } from 'react-router-dom';
-import PriceFilter from "./PriceFilter"
+import { useParams } from "react-router-dom";
+import PriceFilter from "./PriceFilter";
+import DestinationFilter from "./DestinationFilter";
 
 export default function Flights(props) {
-  let {id} = useParams()
+  let { id } = useParams();
   const [minMaxValue, setminMaxValue] = React.useState([200, 500]);
 
-  const [state, setState] = useState({
-    flights: [],
-  });
+  const [flights, setFlights] = useState([]);
+
+  const [filterFlights, setFilterFlights] = useState([]);
+
+  const [selectedDestination, setSelectedDestination] = useState(null);
+
   useEffect(() => {
     axios
       .get(`http://localhost:3000/api/flights/${id}`)
       .then((res) => {
         console.log("axios req data", res.data);
         const flightsData = res.data;
-        setState((prev) => ({ ...prev, flights: flightsData }));
+        setFlights(flightsData);
       })
       .catch((err) => {
         console.log("Error", err);
       });
-  }, []);
-  console.log("STATE", state.flights);
+  }, [id]);
+
+  useEffect(() => {
+    setFilterFlights(
+      flights.filter((flight) => {
+        console.log("selecteddestination", selectedDestination, flight.destination)
+        return (
+          (flight.flightData.price > minMaxValue[0] &&
+          flight.flightData.price < minMaxValue[1]) && (selectedDestination === null || flight.destination === selectedDestination)
+        );
+      })
+    );
+  }, [minMaxValue, flights, selectedDestination]);
+  //console.log("STATE", state.flights);
 
   const handleAdd = (flightObj) => {
-  //  console.log("THIS IS FLIGHT OBJECT+++++", flightObj)
-  //  const { destination } = flightObj; // destructure so it's easy to access
-  //  const { airline, departure_at, expires_at, flight_number, price, return_at } = flightObj.flightData
-  //  const abc = new Date(departure_at).getTime();
-  // let flight_id = `${airline}-${abc}-${flight_number}` // set this as id for local database
-  // console.log(typeof flight_id)
-   let user = JSON.parse(localStorage.getItem("YoLowUser"));
-   const user_id = user.id;
-  //  console.log ("USER----------", user_id)
-   axios
-   .post(`http://localhost:3001/api/flights/${id}/user/favourites/`, { flightObj, user_id })
-   .then(response => {
-     console.log("THIS IS RES+++++++", response)
-   })
-  }
-  console.log("test 25", minMaxValue)
+    //  console.log("THIS IS FLIGHT OBJECT+++++", flightObj)
+    //  const { destination } = flightObj; // destructure so it's easy to access
+    //  const { airline, departure_at, expires_at, flight_number, price, return_at } = flightObj.flightData
+    //  const abc = new Date(departure_at).getTime();
+    // let flight_id = `${airline}-${abc}-${flight_number}` // set this as id for local database
+    // console.log(typeof flight_id)
+    let user = JSON.parse(localStorage.getItem("YoLowUser"));
+    const user_id = user.id;
+    //  console.log ("USER----------", user_id)
+    axios
+      .post(`http://localhost:3001/api/flights/${id}/user/favourites/`, {
+        flightObj,
+        user_id,
+      })
+      .then((response) => {
+        console.log("THIS IS RES+++++++", response);
+      });
+  };
+  // console.log("test 25", minMaxValue)
 
   return (
     <div>
-      <PriceFilter
-      minMaxValue={minMaxValue}
-      setminMaxValue={setminMaxValue}
-      />
+      <PriceFilter minMaxValue={minMaxValue} setminMaxValue={setminMaxValue} />
+      <DestinationFilter setDestination={setSelectedDestination} flightData={filterFlights} />
       <Typography variant="h1"> Flight Deals for {id} </Typography>
 
-      <Container >
+      <Container>
         <Grid container spacing={4}>
-
-          {state.flights.map((flight, index) => {
-            if (flight.flightData.price > minMaxValue[0] && flight.flightData.price < minMaxValue[1]) {
-              return (<Grid item xs={12} sm={6} md={3} key={index}>
+          {filterFlights.map((flight, index) => {
+            return (
+              <Grid item xs={12} sm={6} md={3} key={index}>
                 <Card>
                   <CardContent>
                     <Typography gutterBottom variant="h5">
@@ -81,16 +97,18 @@ export default function Flights(props) {
                     </Typography>
                   </CardContent>
                   <CardContent>
-                    <Button size="small" color="primary" onClick={() => handleAdd(flight)}>
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={() => handleAdd(flight)}
+                    >
                       Favourite
                     </Button>
                   </CardContent>
                 </Card>
-              </Grid>)
-            }
-
-
-})}
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </div>
--- frontend/src/components/flightItem.jsx
+++ frontend/src/components/flightItem.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import className from "classnames";
 import { useState, useEffect} from "react";
 
-export default function FlightItem(props) {
-  return (
-      <lih2 className="t"> {/* rep. entire      </li>
-  );
-}
+// export default function FlightItem(props) {
+//   return (
+//       <lih2 className="t"> {/* rep. entire      </li>
+//   );
+// }
