fab4fddae5cc1be1b327b15baeba00cd860da7b7
Linh Hoang <ellehoang@hotmail.com>
2022-01-25 10:16:03 -0500

    add background image and implement styling for navbar and login

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 /*** material-ui core components ***/
-import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GlobalStyles from '@mui/material/GlobalStyles';
+
 
 export default function Navbar({user, setUser, code}) {
   const history = useHistory();
@@ -16,46 +24,63 @@ export default function Navbar({user, setUser, code}) {
   }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light ">
-      <div className="container-fluid">
-        <Link to="/">Yo-Low Trips</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          { user &&
-            <Button className="nav-link" aria-current="page" component={Link} to="/" variant="text" color="secondary" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "0.9rem" }}>
-              Home
-            </Button>
-          }
-          { user &&
-            // <Button className="nav-link" aria-current="page" component={Link} to="/flights/YYZ" variant="text" color="secondary" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "0.9rem" }}>
-            //     Flight Deals
-            // </Button>
-            <Button className="nav-link" aria-current="page" component={Link} to={`/flights/${code}`} variant="text" color="secondary" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "0.9rem" }}>
-                Flight Deals
-            </Button>
-          }
-          { user &&
-            <Button className="nav-link" aria-current="page" component={Link} to="/favourites" variant="text" color="secondary" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "0.9rem" }}>
-              Favourites
-            </Button>
-          }
-          {
-            !user ?
-              // <div className="d-flex">
-                <Button className="nav-link" aria-current="page" component={Link} to="/login" variant="outlined" color="secondary" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "0.9rem" }}>
-                    Login
-                </Button>
-              // </div> :
-              :
-                <Button onClick={handleLogout} className="nav-item ms-2" variant="outlined" color="secondary" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "0.9rem" }}>
-                    Logout
-                </Button>
-          }
-        </div>
-      </div>
-    </nav>
+    <React.Fragment>
+    <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+        <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+          Yo-Low Trips
+        </Typography>
+          <nav>
+            { user &&
+              <Link
+                variant="button"
+                color="text.primary"
+                to="/"
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Search Origin
+              </Link>
+            }
+            { user &&
+              <Link
+                variant="button"
+                color="text.primary"
+                to={`/flights/${code}`}
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Fight Deals
+              </Link>
+            }
+            { user &&
+              <Link
+                variant="button"
+                color="text.primary"
+                to="/favourites"
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Favourites
+              </Link>
+            }
+            { user &&
+              <Link
+                variant="button"
+                color="text.primary"
+                onClick={handleLogout}
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Logout
+              </Link>
+            }
+          </nav> 
+        </Toolbar>
+      </AppBar>
+     </React.Fragment>
   )
-
 }
